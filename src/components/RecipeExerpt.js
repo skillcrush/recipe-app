0ae0d6ca571671e6a617d41231ac80882605f8c2
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-10-05 16:43:55 -0400

    RecipeFull component

--- src/components/RecipeExerpt.js
+++ src/components/RecipeExerpt.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const RecipeExcerpt = ({ recipe }) => {
+const RecipeExcerpt = ({ recipe, handleSelectRecipe }) => {
+  console.log(handleSelectRecipe);
   return (
     <article key={recipe.id} className='recipe-card'>
       <figure>
@@ -8,7 +9,7 @@ const RecipeExcerpt = ({ recipe }) => {
       </figure>
       <h2>{recipe.title}</h2>
       <p className='flex-spacing'>Description: {recipe.description}</p>
-      <button>View</button>
+      <button onClick={() => handleSelectRecipe(recipe)}>View</button>
     </article>
   );
 };
